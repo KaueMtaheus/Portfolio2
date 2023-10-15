8b3eace98edf6b77346b9a0f6a701badc1a75a80
Kauê Matheus <kauematheus7@hotmail.com>
2023-10-15 17:17:36 -0300

    projetos

--- arquivos/js/script.js
+++ arquivos/js/script.js
@@ -1,1 +1,13 @@
-    
+    //carrosel dos projetos
+
+    $(document).ready(function() {
+        $('.project-container').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          dots: true
+        });
+      });
+      
